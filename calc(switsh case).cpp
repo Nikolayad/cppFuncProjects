22db129fac15d78baef73745194efc04af6d342e
Nikolayad <145016164+Nikolayad@users.noreply.github.com>
2023-09-28 14:30:20 +0300

    create calc (switch case)

---
+++ calc(switsh case).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	float num1,num2;
+	char opiration;
+	cout << "Enter the first number sign and the second number:  ";
+	cin >> num1 >> opiration >> num2;
+	switch (opiration)
+	{
+	case '+' :
+		cout << num1 << '+' << num2 << '=' << (num1 + num2);
+		break;
+	case '-':
+		cout << num1 << '-' << num2 << '=' << (num1 - num2);
+		break;
+	case '*':
+		cout << num1 << '*' << num2 << '=' << (num1 * num2);
+		break;
+	case '/':
+		cout << num1 << '/' << num2 << '=' << (num1 / num2);
+		cout << "division by zero";
+		break;
+	default:
+		cout << "incorrect operation";
+	}
+	
+
+}
+
+
