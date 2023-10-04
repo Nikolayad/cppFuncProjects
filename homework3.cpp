4211806dade848761d10d9ec5e2c65ae8e63913e
Nikolayad <145016164+Nikolayad@users.noreply.github.com>
2023-10-04 22:49:43 +0300

    Add files via upload

---
+++ homework3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    short a1;
+    setlocale(LC_ALL, "");
+    cout << "Сколько минут длился ваш разговор?\n";
+    cin >> a;
+    
+    
+    cout << "Ваш мобильный оператор:(1,2,3)\n";
+    cin >> a1;
+    switch (a1) 
+    {
+    case 1:
+        cout << "Ваш разговор стоит " << a * 4 << " рублей";
+        break;
+    case 2:
+        cout << "Ваш разговор стоит " << a * 6 << " рублей";
+        break;
+    case 3:
+        cout << "Ваш разговор стоит " << a * 7 << " рублей";
+        break;
+    default:
+        cout << "Error";
+    }
+    
+   
+}
